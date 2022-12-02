a0197c70248efe7eaedf5781f6c10e32b1377a7f
pmabasa021 <pmabasa021@student.wethinkcode.co.za>
2022-12-03 00:35:24 +0200

    deploy to heroku

--- Website/__init__.py
+++ Website/__init__.py
@@ -10,7 +10,8 @@ DB_NAME="database.db"
 def create_app():
     app=Flask(__name__)
     app.config['SECRET_KEY']='fkkfdkdfkkdf'
-    app.config['SQLALCHEMY_DATABASE_URI']=f'sqlite:///{DB_NAME}'
+    #app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://utnywdlpjheflw:470e79cd59047fb191735eac75d7eece53d791f806cf1222e95a414a315ca253@ec2-54-82-205-3.compute-1.amazonaws.com:5432/d5k6ei0qb7avmc'
     db.init_app(app)
    
     
